93356b212b0c435d734af6bc147e49d3f278ee04
habib427 <habibsyed427@gmail.com>
2025-05-08 12:02:58 +0500

    event list updated

--- VPN Data.py
+++ VPN Data.py
@@ -2,24 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
- # Set file path
-file_path = "C:/Users/HABIB/PycharmProjects/RandomWebsites/24 to 1st may.csv"  # you can also replace this with input() or sys.argv
 
-#  Load Excel
+file_path = "C:/Users/HABIB/PycharmProjects/RandomWebsites/24 to 1st may.csv"
+
 df = pd.read_csv(file_path)
 
-#  Clean Data
-# Normalize column names
 df.columns = df.columns.str.strip()
 print(" Available columns:", df.columns.tolist())
 
-# Drop rows with missing Event or User ID
 df = df.dropna(subset=["event name", "total users"])
 
-# Convert to lowercase for consistency
 df["event name"] = df["event name"].str.strip().str.lower()
 
-#  nly Specific Events
 selected_events = [
     "splash_screen",
     "main_screen",
@@ -37,7 +31,6 @@ selected_events = [
 
 df = df[df["event name"].isin(selected_events)]
 
-#  Group by Event and count Users
 event_counts = df.groupby("event name")["total users"].count().reset_index()
 event_counts.columns = ["event name", "total users"]
 
@@ -48,7 +41,7 @@ event_counts.to_csv("event_user_counts.csv", index=False)
 print("Cleaned data saved as 'cleaned_event_data24 to 1st may.csv'")
 print("Event count saved as 'event_user_counts.csv'")
 
-# #  Plot the chart
+
 # plt.figure(figsize=(10, 6))
 # plt.bar(event_counts["event name"], event_counts["total users"], color="skyblue")
 # plt.title("Total Users per Event")
